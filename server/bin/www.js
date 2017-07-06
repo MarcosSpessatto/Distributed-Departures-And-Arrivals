92d3f03469a60d2930834f46254dec1e5f9d3a5b
pedrokehl <pedrokehl@hotmail.com>
2017-07-06 13:52:08 -0300

    Fix mongo service file, linux is case-sensitive Fix config Add bower install

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,8 +1,5 @@
 import app from '../app'
-import config from '../config.json';
 
-const server = app.listen(3053, () => {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log(`Server is running on ${config.portListen} ...`);
+app.listen(3053, () => {
+    console.log('Server is running on port 3053 ...');
 });
